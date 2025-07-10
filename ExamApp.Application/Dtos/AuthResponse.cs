e7f6b7d0fe21828555c7c3db1326c7dccc44a088
ameen226 <hosan2662000@gmail.com>
2025-07-10 22:17:58 +0300

    AddingAuthentication&RegisterationInterfaces

---
+++ ExamApp.Application/Dtos/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp.Application.Dtos
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ExamApp.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using ExamApp.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> RegisterAync(RegisterRequest req);
+        Task<AuthResponse> LoginAsync(LoginRequest req);
+    }
+}
