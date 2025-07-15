3349d2e3e909f26f7434c6b7fa7d4e7d14428b0c
ameen226 <hosan2662000@gmail.com>
2025-07-15 22:31:40 +0300

    FixingFolderStructure&AddingSubjectRepositoryClass

--- ExamApp.Infrastructure/Persistence/Repositories/SubjectRepository.cs
+++ ExamApp.Infrastructure/Persistence/Repositories/SubjectRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExamApp.Infrastructure.Repositories
+namespace ExamApp.Infrastructure.Persistence.Repositories
 {
     public class SubjectRepository
     {
